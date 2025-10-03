a1bea2cdfa0ffe3f8ba433ffbe9ed1d59a6f1801
guiziis <guigamoda@hotmail.com>
2025-10-03 16:28:38 -0300

    adiciona terser

--- projeto_03/webpack.config.js
+++ projeto_03/webpack.config.js
@@ -1,9 +1,14 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   mode: 'development',
   module: {
